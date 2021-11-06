8c8a84c4fa17ea88405a6441b73a7f146bfe8eae
Weston Ellis <ellisweston112@gmail.com>
2021-11-05 19:40:09 -0600

    Write test

---
+++ spec/features/hazards/show_spec.rb
@@ -0,0 +1,75 @@
+require "rails_helper"
+
+RSpec.describe 'hazard show page' do
+
+  it 'can display details of the hazard' do
+    # Need code to authenticate user/log them in as user with id 1
+    visit '/hazard/1'
+
+    expect(page).to have_content("Title: Cheyenne, Wyoming")
+    expect(page).to have_content("Description: Je suis comme je suis")
+    expect(page).to have_content("Upvotes: 10")
+    expect(page).to have_content("Downvotes: 1")
+  end
+
+  it 'can upvote the hazard' do
+    # Need code to authenticate user/log them in as user with id 1
+    visit '/hazard/1'
+
+    expect(page).to have_content('Upvotes: 10')
+
+    expect(page).to have_button('Upvote')
+
+    click_on 'Upvote'
+
+    expect(current_path).to eq('/hazard/1')
+    expect(page).to have_content('Upvotes: 11')
+  end
+
+  it 'can downvote the hazard' do
+    # Need code to authenticate user/log them in as user with id 1
+    visit '/hazard/1'
+
+    expect(page).to have_content('Downvotes: 1')
+
+    expect(page).to have_button('Downvote')
+
+    click_on 'Downvote'
+
+    expect(current_path).to eq('/hazard/1')
+    expect(page).to have_content('Downvotes: 1')
+  end
+
+  it 'does not see button to delete hazard when not the owner' do
+    #code to log in as NOT user with id 1
+    visit '/hazard/1'
+
+    expect(page).to_not have_button('Delete Hazard')
+  end
+
+  #test may behave oddly depending on stubing
+  it 'can delete hazard if owner of the hazard' do
+    # Need code to authenticate user/log them in as user with id 1
+    visit '/hazard/1'
+
+    expect(page).to have_content('Delete Hazard')
+
+    click_button('Delete Hazard')
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to_not have_content('Cheyenne, Wyoming')
+  end
+
+  it 'cannot vote on the same hazard twice' do
+    # Need code to authenticate user/log them in as user with id 1    
+    visit '/hazard/1'
+
+    click_on 'Upvote'
+
+    expect(current_path).to eq('/hazard/1')
+
+    expect(page).to_not have_button('Upvote')
+    expect(page).to_not have_button('Downvote')
+  end
+
+end
