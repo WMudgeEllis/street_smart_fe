e773acf55994ccaa6cfe2038234641e9f65f30a0
Ida Olson <idaolson@gmail.com>
2021-11-06 13:50:28 -0600

    Update hazards controller index method

--- app/controllers/hazards_controller.rb
+++ app/controllers/hazards_controller.rb
@@ -1,5 +1,8 @@
 class HazardsController < ApplicationController
   def index
     @hazards= HazardFacade.all_hazards
+    @hazards_coords = @hazards.map do |hazard|
+      [hazard.latitude.to_f, hazard.longitude.to_f, hazard.id]
+    end
   end
 end
