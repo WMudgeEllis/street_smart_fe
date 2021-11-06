bf409623b39fe5a9dd6fb2c12e7d75f8cbbdd32b
Weston Ellis <ellisweston112@gmail.com>
2021-11-06 15:22:04 -0600

    Finish downvote functionality

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,7 +4,7 @@ class VotesController < ApplicationController
     if params[:upvote]
       VoteService.upvote(params[:id], params[:votes].to_i + 1)
     elsif params[:downvote]
-      VoteService.downvote(params[:id], hazard.downvotes)
+      VoteService.downvote(params[:id], params[:votes].to_i + 1)
     end
 
     redirect_to "/hazards/#{params[:id]}"
--- app/services/vote_service.rb
+++ app/services/vote_service.rb
@@ -5,4 +5,10 @@ class VoteService
       f.params['upvote'] = votes
     end
   end
+
+  def self.downvote(id, votes)
+    Faraday.put("https://street-smart-be.herokuapp.com/api/v1/votes/#{id}") do |f|
+      f.params['downvote'] = votes
+    end
+  end
 end
