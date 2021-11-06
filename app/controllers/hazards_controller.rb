420f1149eced3b9bcbaca774e5db77c60026433d
Weston Ellis <ellisweston112@gmail.com>
2021-11-06 10:53:18 -0600

    Add show

--- app/controllers/hazards_controller.rb
+++ app/controllers/hazards_controller.rb
@@ -2,6 +2,9 @@ class HazardsController < ApplicationController
   def index
     ip = request.ip
     @hazards= HazardFacade.all_hazards(ip)
+  end
 
+  def show
+    @hazard = HazardFacade.one_hazard(params[:id])
   end
 end
