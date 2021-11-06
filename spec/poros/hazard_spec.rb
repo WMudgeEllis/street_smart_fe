51c5695ff8d2339061dca8457f3194cf4615f691
Weston Ellis <ellisweston112@gmail.com>
2021-11-06 10:53:44 -0600

    Add vote attributes

--- spec/poros/hazard_spec.rb
+++ spec/poros/hazard_spec.rb
@@ -12,5 +12,7 @@ RSpec.describe Hazard do
     expect(hazards[0].latitude).to eq("-104.8202")
     expect(hazards[0].category).to eq("inanimate")
     expect(hazards[0].user_id).to eq(1)
+    expect(hazards[0].downvotes).to eq(1)
+    expect(hazards[0].upvotes).to eq(10)
   end
 end
