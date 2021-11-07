1611365c039a9aa113e32c142f5ab16a51bf065a
Weston Ellis <ellisweston112@gmail.com>
2021-11-07 15:25:42 -0700

    Add button

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -2,7 +2,7 @@ class UserService
 
   def self.create_user(email)
     conn = Faraday.new do |f|
-      f.params = email
+      f.params['email'] = email
     end
 
     conn.post('https://street-smart-be.herokuapp.com/api/v1/users')
