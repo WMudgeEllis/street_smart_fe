e1ce73f6ea148e291a569fbeadd80bedc77ba7ce
Weston Ellis <ellisweston112@gmail.com>
2021-11-07 15:18:43 -0700

    Add service

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_by(email: auth_hash['info']['email'], uid: auth_hash['uid'])
     session[:user_id] = user.id
+    UserFacade.create_user(user.email)
     redirect_to '/dashboard'
   end
 
