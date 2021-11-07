15b64b473590aeba0eecb266e137f2ddccde56db
Erin Quinn <equinn125@gmail.com>
2021-11-06 19:50:33 -0600

    add dynamic map of near hazards

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :places
+  root "welcome#index"
   get '/dashboard', to: 'hazards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :hazards, only: [:show, :new, :create, :delete]
