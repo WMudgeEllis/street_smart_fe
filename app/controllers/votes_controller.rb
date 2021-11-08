5178d61f4858df5660aa43f58af2019eede094f2
Weston Ellis <ellisweston112@gmail.com>
2021-11-08 09:42:36 -0700

    Add vote facade

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,13 +1,7 @@
 class VotesController < ApplicationController
 
   def update
-    if params[:upvote]
-      VoteService.upvote(params[:id], params[:votes].to_i + 1)
-    elsif params[:downvote]
-      VoteService.downvote(params[:id], params[:votes].to_i + 1)
-    end
-
+    VoteFacade.voter(params)
     redirect_to "/hazards/#{params[:id]}"
   end
-
 end
---
+++ app/facades/vote_facade.rb
@@ -0,0 +1,12 @@
+class VoteFacade
+
+
+  def vote(params)
+    if params[:upvote]
+      VoteService.upvote(params[:id], params[:votes].to_i + 1)
+    elsif params[:downvote]
+      VoteService.downvote(params[:id], params[:votes].to_i + 1)
+    end
+  end
+
+end
