1b5e4e8aaa4c0f296fc699b52b681838189901fe
Weston Ellis <ellisweston112@gmail.com>
2021-11-08 11:17:11 -0700

    Tweak test

--- spec/features/hazards/show_spec.rb
+++ spec/features/hazards/show_spec.rb
@@ -2,9 +2,15 @@ require "rails_helper"
 
 RSpec.describe 'hazard show page' do
 
-  #test may behave oddly depending on stubing
+  #tests may behave oddly depending on stubing
+
+  before :each do
+    @user1 = User.create!(email: 'admin@admin.com')
+    @user2 = User.create!(email: 'nice@sweet.com')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+  end
+
   it 'can display details of the hazard', :vcr do
-    # Need code to authenticate user/log them in as user with id 1
     visit '/hazards/1'
 
     expect(page).to have_content("Title: Cheyenne, Wyoming")
@@ -14,7 +20,6 @@ RSpec.describe 'hazard show page' do
   end
 
   it 'can upvote the hazard', :vcr do
-    # Need code to authenticate user/log them in as user with id 1
     visit '/hazards/1'
 
     expect(page).to have_content('Upvotes: 10')
@@ -28,7 +33,6 @@ RSpec.describe 'hazard show page' do
   end
 
   it 'can downvote the hazard', :vcr do
-    # Need code to authenticate user/log them in as user with id 1
     visit '/hazards/1'
 
     expect(page).to have_content('Downvotes: 1')
@@ -41,7 +45,7 @@ RSpec.describe 'hazard show page' do
     expect(page).to have_content('Downvotes: 2')
   end
 
-  it 'cannot vote on the same hazard twice', :vcr do
+  xit 'cannot vote on the same hazard twice', :vcr do
     # Need code to authenticate user/log them in as user with id 1
     visit '/hazards/1'
 
@@ -53,15 +57,14 @@ RSpec.describe 'hazard show page' do
     expect(page).to_not have_button('Downvote')
   end
 
-  xit 'does not see button to delete hazard when not the owner', :vcr do
-    #code to log in as NOT user with id 1
+  it 'does not see button to delete hazard when not the owner', :vcr do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user2)
     visit '/hazards/1'
 
     expect(page).to_not have_button('Delete Hazard')
   end
 
   it 'can delete hazard if owner of the hazard', :vcr do
-    # Need code to authenticate user/log them in as user with id 1
     visit '/hazards/1'
 
     expect(page).to have_button('Delete Hazard')
