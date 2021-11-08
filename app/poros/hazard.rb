8bdcf361f46b217c318604ee75a943197f17d568
Weston Ellis <ellisweston112@gmail.com>
2021-11-08 11:08:37 -0700

    Update poro

--- app/poros/hazard.rb
+++ app/poros/hazard.rb
@@ -1,5 +1,5 @@
 class Hazard
-  attr_reader :id, :type, :title, :description, :longitude, :latitude, :category, :user_id, :upvotes, :downvotes
+  attr_reader :id, :type, :title, :description, :longitude, :latitude, :category, :upvotes, :downvotes, :email
   def initialize(data)
     @id = data[:id].to_i
     @type = data[:type]
@@ -8,9 +8,8 @@ class Hazard
     @longitude = data[:attributes][:longitude]
     @latitude = data[:attributes][:latitude]
     @category = data[:attributes][:category]
-    @user_id = data[:attributes][:user_id].to_i
-    # @vote_data = data[:attributes][:vote_data]
     @downvotes = data[:attributes][:vote_data][:downvote].to_i
     @upvotes = data[:attributes][:vote_data][:upvote].to_i
+    @email = data[:attributes][:user_email]
   end
 end
--- spec/poros/hazard_spec.rb
+++ spec/poros/hazard_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe Hazard do
     expect(hazards[0].type).to eq("hazard")
     expect(hazards[0].title).to eq("Cheyenne, Wyoming")
     expect(hazards[0].description).to eq("Je suis comme je suis")
-    expect(hazards[0].longitude).to eq("41.1400")
-    expect(hazards[0].latitude).to eq("-104.8202")
+    expect(hazards[0].longitude).to eq(-104.8202)
+    expect(hazards[0].latitude).to eq(41.1400)
     expect(hazards[0].category).to eq("inanimate")
-    expect(hazards[0].user_id).to eq(1)
     expect(hazards[0].downvotes).to eq(1)
     expect(hazards[0].upvotes).to eq(10)
+    expect(hazards[0].email).to eq('admin@admin.com')
   end
 end
