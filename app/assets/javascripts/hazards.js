fcbb20c06d64d0956dc971f694285d93ea7c89bc
Ida Olson <idaolson@gmail.com>
2021-11-09 13:51:45 -0700

    Add function initHazardMap2 to hazards javascript

--- app/assets/javascripts/hazards.js
+++ app/assets/javascripts/hazards.js
@@ -35,17 +35,9 @@ function initHazardIndexMap(locations) {
 }
 // Check on use of place below
 
-function initMap2() {
-    var lat = document.getElementById('place_latitude').value;
-    var lng = document.getElementById('place_longitude').value;
-
-    // if not defined create default position
-    if (!lat || !lng){
-        lat=51.5;
-        lng=-0.125;
-        document.getElementById('place_latitude').value = lat;
-        document.getElementById('place_longitude').value = lng;
-    }
+function initHazardMap2(lat, lng) {
+    document.getElementById('latitude').value = lat;
+    document.getElementById('longitude').value = lng;
 
     var myCoords = new google.maps.LatLng(lat, lng);
     var mapOptions = {
@@ -53,7 +45,7 @@ function initMap2() {
         zoom: 14
     };
 
-    var map = new google.maps.Map(document.getElementById('map2'), mapOptions);
+    var map = new google.maps.Map(document.getElementById('mapHazard2'), mapOptions);
 
     var marker = new google.maps.Marker({
         position: myCoords,
@@ -64,23 +56,23 @@ function initMap2() {
 
     // refresh marker position and recenter map on marker
     function refreshMarker(){
-        var lat = document.getElementById('place_latitude').value;
-        var lng = document.getElementById('place_longitude').value;
+        var lat = document.getElementById('latitude').value;
+        var lng = document.getElementById('longitude').value;
         var myCoords = new google.maps.LatLng(lat, lng);
         marker.setPosition(myCoords);
         map.setCenter(marker.getPosition());
     }
     // when input values change call refreshMarker
-    document.getElementById('place_latitude').onchange = refreshMarker;
-    document.getElementById('place_longitude').onchange = refreshMarker;
+    document.getElementById('latitude').onchange = refreshMarker;
+    document.getElementById('longitude').onchange = refreshMarker;
 
     // when marker is dragged update input values
     marker.addListener('drag', function() {
         latlng = marker.getPosition();
         newlat=(Math.round(latlng.lat()*1000000))/1000000;
         newlng=(Math.round(latlng.lng()*1000000))/1000000;
-        document.getElementById('place_latitude').value = newlat;
-        document.getElementById('place_longitude').value = newlng;
+        document.getElementById('latitude').value = newlat;
+        document.getElementById('longitude').value = newlng;
     });
 
     // When drag ends, center (pan) the map on the marker position
@@ -89,4 +81,3 @@ function initMap2() {
     });
 
 }
-// console.log(map, 'map')
