f2d758f4126d5e2e6ff7651398ad4947b0ebe95c
Ida Olson <idaolson@gmail.com>
2021-11-09 14:37:45 -0700

    Remove places from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :places
   root "welcome#index"
   get '/dashboard', to: 'hazards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
