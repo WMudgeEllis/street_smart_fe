c49c3fa543cef1f509acb4e3b2a9d6cc8e7d16b0
Weston Ellis <ellisweston112@gmail.com>
2021-11-10 11:25:41 -0700

    Change param name

--- app/services/vote_service.rb
+++ app/services/vote_service.rb
@@ -3,14 +3,14 @@ class VoteService
   def self.upvote(id, votes, email)
     Client.conn.put("/api/v1/votes/#{id}") do |f|
       f.params['upvote'] = votes
-      f.params['user_email'] = email
+      f.params['email'] = email
     end
   end
 
   def self.downvote(id, votes, email)
     Client.conn.put("/api/v1/votes/#{id}") do |f|
       f.params['downvote'] = votes
-      f.params['user_email'] = email
+      f.params['email'] = email
     end
   end
 end
